bc8c08d07a129a10d3d6c06cd71c2ba988488b29
Pascal Schulze <p.schulze@sap.com>
2020-08-21 21:46:27 +0200

    fix

--- algorithm_evaluation/generator.py
+++ algorithm_evaluation/generator.py
@@ -4,7 +4,7 @@ import sys
 assert len(sys.argv) == 4
 sources = sys.argv[1]
 sequences = sys.argv[2]
-num_of_files = sys.argv[3]
+num_of_files = int(sys.argv[3])
 
 for i in range (1, num_of_files+1):
     print(f"Generate file with overlapping: {i}/{num_of_files}")
